c6c8bc30767250238abfb38d6457e3412e504100
kukita <ku.k669500@gmail.com>
2019-03-30 10:52:16 +0900

    動いた！！！！

--- app/src/main/java/kktyu/xyz/coroutine_study/HttpUtil.kt
+++ app/src/main/java/kktyu/xyz/coroutine_study/HttpUtil.kt
@@ -5,7 +5,7 @@ import okhttp3.Request
 
 class HttpUtil {
     //叩きたいREST APIのURLを引数とします
-    fun httpGET1(url : String): String? {
+    fun httpGET1(url: String): String? {
         val client = OkHttpClient()
         val request = Request.Builder()
             .url(url)
